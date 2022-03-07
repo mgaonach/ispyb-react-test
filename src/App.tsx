422ed0a07f525f2edf0e693a711ba014386048f1
Stuart Fisher <stuart.fisher@esrf.fr>
2022-03-07 17:35:35 +0100

    loading

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,13 @@ import routes from 'routes';
 function Loading() {
   return (
     <section className="loading">
-      <Container className="text-center m-5">
-        <Spinner animation="border" role="status" variant="primary">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-        <div className="text-primary">Loading ...</div>
+      <Container className="text-center">
+        <div className="m-5">
+          <Spinner animation="border" role="status" variant="primary">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+          <div className="text-primary">Loading ...</div>
+        </div>
       </Container>
     </section>
   );
