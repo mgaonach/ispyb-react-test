a921f3d29ec8bece5cc91a0f06c3afd918aa1245
Stuart Fisher <stuart.fisher@esrf.fr>
2022-09-27 09:58:22 +0200

    update error parsing

--- src/components/NetworkErrorPage.tsx
+++ src/components/NetworkErrorPage.tsx
@@ -1,25 +1,13 @@
-import { NetworkErrorBoundary, NetworkError } from 'rest-hooks';
-
-function ErrorPageFactory({ message }: { message?: string }) {
-  return function ({ error }: { error: NetworkError }) {
-    console.log('Error Page', error);
-    return error.status === 404 ? (
-      <span>{message}</span>
-    ) : (
-      <span>An error occured: {error.message}</span>
-    );
-  };
-}
+import { NetworkErrorBoundary } from 'rest-hooks';
+import { ParsedError } from 'components/ParsedError';
 
 export default function NetworkErrorPage({
   children,
-  message,
 }: {
   children: JSX.Element;
-  message?: string;
 }) {
   return (
-    <NetworkErrorBoundary fallbackComponent={ErrorPageFactory({ message })}>
+    <NetworkErrorBoundary fallbackComponent={ParsedError}>
       {children}
     </NetworkErrorBoundary>
   );
