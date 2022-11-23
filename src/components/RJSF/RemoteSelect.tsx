0c9b6ef41b921cc1541de91323ee367e90112f51
Maël Gaonach <mael.gaonach@esrf.fr>
2022-11-23 14:23:40 +0100

    fix resource paginated

--- src/components/RJSF/RemoteSelect.tsx
+++ src/components/RJSF/RemoteSelect.tsx
@@ -2,7 +2,7 @@ import { createRef, Suspense, useCallback, useEffect } from 'react';
 import { Form } from 'react-bootstrap';
 import { useSuspense } from 'rest-hooks';
 import { WidgetProps, UIOptionsType, EnumOptionsType } from '@rjsf/utils';
-import PaginatedResource from '../../api/resources/Paginated';
+import PaginatedResource from '../../api/resources/Base/Paginated';
 
 interface SelectOptions<T = any, F = any> extends UIOptionsType<T, F> {
   resource: typeof PaginatedResource;
