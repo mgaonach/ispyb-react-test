ea60ea03e1e35382b7937aeda3c60e165b775218
Maël Gaonach <mael.gaonach@esrf.fr>
2022-12-05 12:03:52 +0100

    metadata popover

--- src/components/Events/Metadata.tsx
+++ src/components/Events/Metadata.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Popover, OverlayTrigger } from 'react-bootstrap';
 
 export interface IMetadataItemProps {
   title: string;
@@ -39,14 +39,24 @@ export function MetadataCol(props: IMetadataProps) {
 }
 
 export function MetadataItem(props: IMetadataItemProps) {
+  const popover = (
+    <Popover className="metadata-popover">
+      <Popover.Header as="h3">{props.title}</Popover.Header>
+      <Popover.Body>
+        {props.content} <span className="text-primary">{props.unit}</span>
+      </Popover.Body>
+    </Popover>
+  );
   return (
     <React.Fragment key={props.title}>
-      {(props.test === undefined || props.test) && (
-        <div className="mx-1 mb-2 p-2 bg-light text-truncate">
-          <span className="text-primary">{props.title}</span>: {props.content}{' '}
-          <span className="text-primary">{props.unit}</span>
-        </div>
-      )}
+      <OverlayTrigger trigger="hover" placement="auto" overlay={popover}>
+        {(props.test === undefined || props.test) && (
+          <div className="mx-1 mb-2 p-2 bg-light text-truncate">
+            <span className="text-primary">{props.title}</span>: {props.content}{' '}
+            <span className="text-primary">{props.unit}</span>
+          </div>
+        )}
+      </OverlayTrigger>
     </React.Fragment>
   );
 }
